ff8e7683855ae75ddc7ad3e575a32e6081847a9b
siddhey44 <138798919+siddhey44@users.noreply.github.com>
2024-08-09 04:00:31 +0530

    Add files via upload
    check the eligibility of voter can vote or not

---
+++ voting.py
@@ -0,0 +1,8 @@
+name=str(input("enter your name :"))
+print("Good name",name)
+age=int(input("enter your age :"))
+print("your age is",age)
+if (age>=18) :
+    print(name,"you are eligible to vote")
+else :
+    print(name,"you are not eligible to vote")
